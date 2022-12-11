997abac5df074206bcf3cf2ec6ababc6c22e6fa4
TryCatch <sgykim36@gmail.com>
2022-12-11 21:25:55 +0900

    인게임 UI 폴리싱 (스코어 위치 보정 기능)

--- Client/Assets/Scripts/UI/UI_ScoreText.cs
+++ Client/Assets/Scripts/UI/UI_ScoreText.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class UI_ScoreText : MonoBehaviour
 {
+    public Canvas mainCanvas;
     public TMPro.TMP_Text scoreText;
     public TMPro.TextMeshProEffect tmpEffect;
     // Start is called before the first frame update
     void Awake()
     {
         GameBoard.onChangeScore += OnChangeScore;
+        GameBoard.onInitBoard += OnInitBoard;
     }
 
     private void OnDestroy()
     {
         GameBoard.onChangeScore -= OnChangeScore;
+        GameBoard.onInitBoard -= OnInitBoard;
     }
 
     void OnChangeScore(int getScore, int totalScore)
@@ -25,9 +28,11 @@ public class UI_ScoreText : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnInitBoard()
     {
-        
+        //Vector3 fixedThisPos = this.transform.position;
+        //float offsetY = mainCanvas.GetComponent<RectTransform>().rect.height * 0.027f;
+        //fixedThisPos.y = GameBoard.BoardScreenCoordTopY + offsetY;
+        //this.transform.position = fixedThisPos;
     }
 }
